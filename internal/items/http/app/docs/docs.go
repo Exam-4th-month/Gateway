f514947779e4609373bc824c8569e23e513edf83
Abdulaziz Kholikulov <phoenixaax2011@gmail.com>
2024-08-16 17:54:43 +0500

    swagger updated

--- internal/items/http/app/docs/docs.go
+++ internal/items/http/app/docs/docs.go
@@ -2571,8 +2571,8 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0",
-	Host:             "localhost:8080",
+	Version:          "1.03.67.83.145",
+	Host:             "3.67.83.145:8080",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
 	Title:            "# Personal Finance Tracker",
